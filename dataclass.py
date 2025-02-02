216466219a9b28a1ed9d728526a9d4785a1407b8
tokihiko-tsurumoto <tokihiko-tsurumoto@cybozu.co.jp>
2025-02-02 17:19:17 +0900

    feat: showメソッドのフォーマットを修正し、クラス変数jの変更を追加

--- dataclass.py
+++ dataclass.py
@@ -16,7 +16,8 @@ class C:
     k = 0
 
     def show(self) -> str:
-        return f'i: {self.i}, j: {self.j}, k: {self.k}'
+        return f"i: {self.i}, j: {self.j}, k: {self.k}"
+
 
 # インスタンス変数であるiに1を代入してインスタンスを生成
 c = C(1)
@@ -25,5 +26,11 @@ c = C(1)
 # c.i = 2
 # 例外として、object.__setattr__()を使うことで変更できる（コンストラクタでもobject.__setattr__()が使われている）
 object.__setattr__(c, "i", 2)
+
+
+# C.jに代入することでクラス変数を変更できる
+# c.jやC().jに代入した場合、インスタンス変数として新たにjが生成される（この例ではfrozen=Trueにしているので代入できない）
+C.j = 1
+
 
 print(c.show())
