8a3ead92e79d6a59b197303b37a4e9c1e37d72b0
tokihiko-tsurumoto <tokihiko-tsurumoto@cybozu.co.jp>
2025-02-03 09:42:33 +0900

    feat: Pydanticを使用したUserクラスを新規作成し、インスタンス生成時にユーザー数をカウント

---
+++ practice_pydantic_dataclasses.py
@@ -0,0 +1,46 @@
+from typing import ClassVar
+
+from pydantic import ValidationError
+from pydantic.dataclasses import dataclass
+
+
+@dataclass(frozen=False)
+class User:
+    # インスタンス変数
+    id: int
+    name: str
+    age: int
+
+    # クラス変数
+    user_count: ClassVar[int] = 0
+
+    # カスタムの処理を追加したい場合は__post_init__()をオーバーライド
+    def __post_init__(self, **data):
+        # BaseModelのコンストラクタ（バリデーションやデフォルト値の適用などを行う）を呼び出す
+        super().__init__(**data)
+        User.user_count += 1
+        print(f"user_count: {User.user_count}")
+
+
+# 正しいデータでインスタンスを作成
+try:
+    user = User(id=1, name="Alice", age=20)
+    print(user)
+except ValidationError as e:
+    print(e)
+
+# 自動変換可能なデータでインスタンスを作成
+try:
+    # 以下のように可変長引数でデータを渡すこともできる
+    data = {"id": "2", "name": "John", "age": "20"}
+    user = User(**data)
+    print(user)
+except ValidationError as e:
+    print(e)
+
+# 不正なデータでインスタンスを作成
+try:
+    user = User(id="3", name="Bob", age="twenty")
+    print(user)
+except ValidationError as e:
+    print(e)
